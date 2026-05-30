ce71f950202bf44b3b3e0b2bb59fca2dd5fd8a6f
krishnaabhiram6 <krishnaabhiram666@gmail.com>
2026-05-30 12:16:00 +0530

    ADDED EXTRA CODE

--- Week1/employee_management.py
+++ Week1/employee_management.py
@@ -1,7 +1,10 @@
 employees = [
     {"id": 101, "name": "Krishna", "department": "IT"},
-    {"id": 102, "name": "Ravi", "department": "HR"}
+    {"id": 102, "name": "Ravi", "department": "HR"},
+    {"id": 103, "name": "Sita", "department": "Finance"}
 ]
 
 for employee in employees:
     print(employee)
+
+print("Total Employees:", len(employees))
